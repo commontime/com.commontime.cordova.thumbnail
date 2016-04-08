c7be667eadc5df8d178a72266a0812deee8ab394
Gary Meehan <wingstand@googlemail.com>
2016-04-08 10:41:35 +0100

    Skeleton plugin files for the WP8 plugin.

---
+++ src/wp8/ThumbnailPlugin.cs
@@ -0,0 +1,10 @@
+namespace WPCordovaClassLib.Cordova.Commands
+{
+  public class ThumbnailPlugin : BaseCommand
+  {
+    public void makeThumbnail(string args)
+    {
+      // TODO: implement
+    }
+  }
+}
